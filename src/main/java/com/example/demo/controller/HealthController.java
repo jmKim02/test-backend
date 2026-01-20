67d3abfad1b035807d8b61d455c95fe4bada603b
Jongmin Kim <jmkim990226@gmail.com>
2026-01-20 11:44:28 +0900

    test

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -26,7 +26,7 @@ public class HealthController {
     // 🆕 새로 추가
     @GetMapping("/api/version")
     public String version() {
-        return "v2.0.3";
+        return "v2.0.4";
     }
 
     // 🆕 내부 클래스 추가
